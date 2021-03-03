816f9b166199569e14ab2a760525a5bcf2360008
Zayan-Zakir <64923451+Zayan-Zakir@users.noreply.github.com>
2021-03-03 12:30:22 +0500

    quiz assignement
    quiz assignement

---
+++ question.js
@@ -0,0 +1,76 @@
+let questions = [
+    {
+      
+        num: 1,
+        question: "what does HTML stand for?",
+        answer :   "Hyper Text Markup Languange",
+        option :[
+            "Hyper Text Processor",
+            "Hyper Text Markup Languange",
+            "Hyper Text Multiple Language",
+            "Hyper Tool Multi Language"
+
+        ]
+        
+    },
+
+    {
+      
+        num: 2,
+        question: "what does CSS stand for?",
+        answer : "Cascading Style Sheet",
+        option :[
+            "Common Style Sheet",
+            "Colorful Style Sheet",
+            "Computer Style Sheet",
+            "Cascading Style Sheet"
+
+        ]
+        
+    },
+
+    {
+      
+        num: 3,
+        question: "what does PHP stand for?",
+        answer  :  "Hypertext preprocessor",
+        option :[
+            "Hypertext preprocessor",
+            "Hypertext Programing",
+            "Hypertext Preprograming",
+            "Hometext preprocessor",
+
+        ]
+        
+    },
+
+    {
+      
+        num: 4,
+        question: "what does SQL stand for?",
+        answer  :   "Structured Query Language",
+        option :[
+            "Stylish Question Language",
+            "Stylesheet Query Languange",
+            "Statment Question Language",
+            "Structured Query Language"
+
+        ]
+        
+    },
+    
+    {
+      
+        num: 5,
+        question: "what does XML stand for?",
+        answer  :  "eXtensible Markup Languange",
+        option :[
+            "eXtensible Markup Languange",
+            "eXecutable Multiple Language",
+            "eXtra Multi-Program Languange",
+            "eXamine Multiple Language"
+
+        ]
+        
+    }
+];
